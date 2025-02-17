6fb2cc97a0cc732fd0e21ddc75e50175767ba40a
LittleTr0ll <fusetea2006@mail.ru>
2025-02-17 21:15:53 +0500

    login.php

--- pages/login.php
+++ pages/login.php
@@ -5,12 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="css/styleL.css">
+    <link rel="stylesheet" href="assets/styles/styleL.css">
 </head>
 
 <body>
-    <img src="images/leaf.png" class="leafone" alt="leaf">
-    <img src="images/leaf1.png" class="leaftwo" alt="leaf">
+    <img src="images/leaf.png" class="assets/photos/leafone" alt="leaf">
+    <img src="images/leaf1.png" class="assets/photos/leaftwo" alt="leaf">
     <div class="container">
         <h1 class="center">LOGIN</h1>
         <form action="login_check.php" method="POST">
